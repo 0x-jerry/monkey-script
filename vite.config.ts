d1247045955e358b2d4d39f225fc29b54f431d2b
Jerry Wang <x.jerry.wang@gmail.com>
2021-08-08 18:25:30 +0800

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig((conf) => {
   const isDev = conf.mode == 'dev'
-  console.log(`serve at: file://${process.cwd()}/dev.js`)
+  console.log(`serve at: file://${process.cwd()}/dev.user.js`)
 
   return {
     build: {
