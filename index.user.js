0e02db93878915c611191603290162c5b7809a2f
Jerry Wang <x.jerry.wang@gmail.com>
2023-07-01 22:47:49 +0800

    chore: release v0.3.1

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Useful Script
 // @namespace    http://tampermonkey.net/
-// @version      0.3.0
+// @version      0.3.1
 // @description  try to take over the world!
 // @author       0x-jerry
 // @match        http://*/*
